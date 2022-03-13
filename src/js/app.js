b3dceb252e500d340646e4454513c8c390804701
levinlainez <fidelina.aguirre1970@gmail.com>
2022-03-12 18:08:40 -0600

    ordenar tabla

--- src/js/app.js
+++ src/js/app.js
@@ -63,6 +63,7 @@ function ver(e) {
           data.push(cells[i].innerHTML);
         }
       }
+      
 
       contaGanador = parseInt(data.slice(1, 2));
       nombreJugador = data.slice(0, 1);
@@ -77,6 +78,43 @@ function ver(e) {
     }
   });
 }
+
+$('th').click(function() {
+  var table = $(this).parents('table').eq(0)
+  var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+  this.asc = !this.asc
+  if (!this.asc) {
+    rows = rows.reverse()
+  }
+  for (var i = 0; i < rows.length; i++) {
+    table.append(rows[i])
+  }
+  setIcon($(this), this.asc);
+})
+
+function comparer(index) {
+  return function(a, b) {
+    var valA = getCellValue(a, index),
+      valB = getCellValue(b, index)
+    return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
+  }
+}
+
+function getCellValue(row, index) {
+  return $(row).children('td').eq(index).html()
+}
+
+function setIcon(element, asc) {
+  $("th").each(function(index) {
+    $(this).removeClass("sorting");
+    $(this).removeClass("asc");
+    $(this).removeClass("desc");
+  });
+  element.addClass("sorting");
+  if (asc) element.addClass("asc");
+  else element.addClass("desc");
+}
+
 //grafico de ganador y perdedor
 function graficos(contaGanador, contaPerdedor, nombreJugador) {
   google.charts.load("current", { packages: ["corechart"] });
